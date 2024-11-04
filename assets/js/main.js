7124bb7b4913101ae36f665cd4ab6ab02b0806fa
erin210 <yourweb210@gmail.com>
2024-11-04 11:07:41 +0800

    補完畫面

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,6 @@ $(document).ready(function(){
         searchTop.classList.add("fade-out");
 
         searchTop.style.display = "none";
-        
-        // // 等待淡出動畫結束後隱藏按鈕（0.5秒是對應的動畫時間）
-        // setTimeout(() => {
-        // }, 500);
     });
-
 
 });
